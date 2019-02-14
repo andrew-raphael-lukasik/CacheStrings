da6378b46868cea7a964912474cf06c008c69bc3
Andrew Raphael Lukasik <andrew.r.lukasik@gmail.com>
2019-02-14 23:17:21 +0100

    added: hash function, initStep

--- CacheIntString.cs
+++ CacheIntString.cs
@@ -1,36 +1,43 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-
 using UnityEngine;
-
-public class CacheIntString
+public sealed class CacheIntString
 {
 	#region FIELDS
 
-	[SerializeField] protected Dictionary<int,string> _values;
-	System.Func<int,string> _intToString;
+	Dictionary<int,string> _values;
+	Func<int,string> _intToString;
+	Func<int,int> _hashFunction;
 
 	#endregion
 	#region CONSTRUCTORS
 
-	public CacheIntString ( System.Func<int,string> intToString  ) : this( intToString , 100 ) {}
-	public CacheIntString ( System.Func<int,string> intToString , int initialDictCapacity )
+	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString  ) : this( hashFunction , intToString , 100 ) {}
+	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString , int initialDictCapacity )
 	{
+		UnityEngine.Assertions.Assert.IsNotNull( hashFunction );
 		UnityEngine.Assertions.Assert.IsNotNull( intToString );
 		_values = new Dictionary<int,string>( initialDictCapacity );
+		_hashFunction = hashFunction;
 		_intToString = intToString;
 	}
-	public CacheIntString ( System.Func<int,string> intToString , int initMin , int initMax )
+	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString , int initMin , int initMax , int initStep )
 	{
+		UnityEngine.Assertions.Assert.IsNotNull( hashFunction );
 		UnityEngine.Assertions.Assert.IsNotNull( intToString );
-		_values = new Dictionary<int,string>( initMax-initMin );
-		for( int i=initMin ; i<=initMax ; i++ )
-		{
-			string str = intToString( i );
-			UnityEngine.Assertions.Assert.IsNotNull( str );
-			_values.Add( i , str );
-		}
+		_hashFunction = hashFunction;
 		_intToString = intToString;
+		_values = new Dictionary<int,string>( initMax-initMin );
+		for( int key=initMin ; key<=initMax ; key+=initStep )
+		{
+			int hash = hashFunction( key );
+			string str = intToString( hash );
+			UnityEngine.Assertions.Assert.IsNotNull( str );
+			try {
+				_values.Add( hash , str );
+			}
+			catch( Exception ex ) { Debug.LogWarning($"Redundant key: { key }, where hash: { hash }"); Debug.LogException( ex ); }
+		}
 	}
 	
 	#endregion
@@ -41,11 +48,12 @@ public class CacheIntString
 		get
 		{
 			string result;
-			if( _values.TryGetValue( key , out result )==false )//fail safe
+			int hash = _hashFunction( key );
+			if( _values.TryGetValue( hash , out result )==false )//fail safe
 			{
-				result = _intToString( key );
+				result = _intToString( hash );
 				UnityEngine.Assertions.Assert.IsNotNull( result );
-				_values.Add( key , result );
+				_values.Add( hash , result );
 			}
 			return result;
 		}
