acbb4fcc77fe3b966317dcca7e70caa48967281e
Andrew Raphael Lukasik <andrew.r.lukasik@gmail.com>
2019-02-15 00:23:59 +0100

    turned full generic, added new variants

--- CacheIntString.cs
+++ CacheIntString.cs
@@ -1,63 +1,129 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-public sealed class CacheIntString
+
+public class CacheStrings <KEY,HASH>
 {
-	#region FIELDS
+	protected Dictionary<HASH,string> _table;
+	protected Func<HASH,string> _hashToString;
+	protected Func<KEY,HASH> _hashFunction;
 
-	Dictionary<int,string> _values;
-	Func<int,string> _intToString;
-	Func<int,int> _hashFunction;
-
-	#endregion
-	#region CONSTRUCTORS
-
-	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString  ) : this( hashFunction , intToString , 100 ) {}
-	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString , int initialDictCapacity )
+	public CacheStrings ( Func<KEY,HASH> hashFunction , Func<HASH,string> hashToString  ) : this( hashFunction , hashToString , 100 ) {}
+	public CacheStrings ( Func<KEY,HASH> hashFunction , Func<HASH,string> hashToString , int initialDictCapacity )
 	{
 		UnityEngine.Assertions.Assert.IsNotNull( hashFunction );
-		UnityEngine.Assertions.Assert.IsNotNull( intToString );
-		_values = new Dictionary<int,string>( initialDictCapacity );
+		UnityEngine.Assertions.Assert.IsNotNull( hashToString );
+		_table = new Dictionary<HASH,string>( initialDictCapacity );
 		_hashFunction = hashFunction;
-		_intToString = intToString;
-	}
-	public CacheIntString ( Func<int,int> hashFunction , Func<int,string> intToString , int initMin , int initMax , int initStep )
-	{
-		UnityEngine.Assertions.Assert.IsNotNull( hashFunction );
-		UnityEngine.Assertions.Assert.IsNotNull( intToString );
-		_hashFunction = hashFunction;
-		_intToString = intToString;
-		_values = new Dictionary<int,string>( initMax-initMin );
-		for( int key=initMin ; key<=initMax ; key+=initStep )
-		{
-			int hash = hashFunction( key );
-			string str = intToString( hash );
-			UnityEngine.Assertions.Assert.IsNotNull( str );
-			try {
-				_values.Add( hash , str );
-			}
-			catch( Exception ex ) { Debug.LogWarning($"Redundant key: { key }, where hash: { hash }"); Debug.LogException( ex ); }
-		}
+		_hashToString = hashToString;
 	}
 	
-	#endregion
-	#region OPERATORS
-
-	public string this [ int key ]
+	public string this [ KEY key ]
 	{
 		get
 		{
 			string result;
-			int hash = _hashFunction( key );
-			if( _values.TryGetValue( hash , out result )==false )//fail safe
+			HASH hash = _hashFunction( key );
+			if( _table.TryGetValue( hash , out result )==false )//fail safe
 			{
-				result = _intToString( hash );
+				result = _hashToString( hash );
 				UnityEngine.Assertions.Assert.IsNotNull( result );
-				_values.Add( hash , result );
+				_table.Add( hash , result );
 			}
 			return result;
 		}
 	}
 
-	#endregion
+}
+
+public sealed class CacheIntString : CacheStrings<int,int>
+{
+	public CacheIntString
+	(
+		Func<int,int> hashFunction ,
+		Func<int,string> hashToString ,
+		int initMin ,
+		int initMax ,
+		int initStep
+	)
+		: base( hashFunction , hashToString , initMax-initMin )
+	{
+		for( int key=initMin ; key<=initMax ; key+=initStep )
+		{
+			int hash = hashFunction( key );
+			string str = hashToString( hash );
+			UnityEngine.Assertions.Assert.IsNotNull( str );
+			if( _table.ContainsKey( hash )==false ) _table.Add( hash , str );
+			else Debug.LogWarning( $"Redundant key: { key }, where hash: { hash }" );
+		}
+	}
+}
+
+public sealed class CacheUIntString : CacheStrings<uint,uint>
+{
+	public CacheUIntString
+	(
+		Func<uint,uint> hashFunction ,
+		Func<uint,string> hashToString ,
+		uint initMin ,
+		uint initMax ,
+		uint initStep
+	)
+		: base( hashFunction , hashToString , Convert.ToInt32(initMax-initMin) )
+	{
+		for( uint key=initMin ; key<=initMax ; key+=initStep )
+		{
+			uint hash = hashFunction( key );
+			string str = hashToString( hash );
+			UnityEngine.Assertions.Assert.IsNotNull( str );
+			if( _table.ContainsKey( hash )==false ) _table.Add( hash , str );
+			else Debug.LogWarning( $"Redundant key: { key }, where hash: { hash }" );
+		}
+	}
+}
+
+public sealed class CacheULongString : CacheStrings<ulong,ulong>
+{
+	public CacheULongString
+	(
+		Func<ulong,ulong> hashFunction ,
+		Func<ulong,string> hashToString ,
+		ulong initMin ,
+		ulong initMax ,
+		ulong initStep
+	)
+		: base( hashFunction , hashToString , Convert.ToInt32(initMax-initMin) )
+	{
+		for( ulong key=initMin ; key<=initMax ; key+=initStep )
+		{
+			ulong hash = hashFunction( key );
+			string str = hashToString( hash );
+			UnityEngine.Assertions.Assert.IsNotNull( str );
+			if( _table.ContainsKey( hash )==false ) _table.Add( hash , str );
+			else Debug.LogWarning( $"Redundant key: { key }, where hash: { hash }" );
+		}
+	}
+}
+
+public sealed class CacheDoubleString : CacheStrings<double,double>
+{
+	public CacheDoubleString
+	(
+		Func<double,double> hashFunction ,
+		Func<double,string> hashToString ,
+		double initMin ,
+		double initMax ,
+		double initStep
+	)
+		: base( hashFunction , hashToString , Convert.ToInt32(initMax-initMin) )
+	{
+		for( double key=initMin ; key<=initMax ; key+=initStep )
+		{
+			double hash = hashFunction( key );
+			string str = hashToString( hash );
+			UnityEngine.Assertions.Assert.IsNotNull( str );
+			if( _table.ContainsKey( hash )==false ) _table.Add( hash , str );
+			else Debug.LogWarning( $"Redundant key: { key }, where hash: { hash }" );
+		}
+	}
 }
